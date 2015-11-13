7b71ceae13f0c5cfab7addd2669ca043042f3147
Dave Anderson <danderson@ncarb.org>
2015-11-13 14:49:22 +0000

    decorate enums

--- modules/common/enum.js
+++ modules/common/enum.js
@@ -6,43 +6,54 @@
         .service('enumService', enumService);
         
     function enumService() {
-        var professionalStatus = [
+        var professionalStatus = decorate([
             { value: 'Principal', text: 'Principal' },
             { value: 'CorporateOfficer', text: 'Corporate Officer' },
             { value: 'Employee', text: 'Employee' },
             { value: 'Other', text: 'Other' }
-        ];
+        ]);
         
-        var phoneTypes = [
+        var phoneTypes = decorate([
             { value: 'Cell', text: 'Cell' },
             { value: 'Home', text: 'Home' },
             { value: 'Work', text: 'Work' }
-        ];
+        ]);
         
-        var salutations = [
+        var salutations = decorate([
             { value: 'mr', text: 'Mr.' },
             { value: 'ms', text: 'Ms.' },
             { value: 'mrs', text: 'Mrs.' },
             { value: 'miss', text: 'Miss' },
             { value: 'dr', text: 'Dr.' }
-        ];
+        ]);
 
-        var suffixes = [
+        var suffixes = decorate([
             { value: 'junior', text: 'Jr.' },
             { value: 'senior', text: 'Sr.' },
             { value: 'second', text: 'II' },
             { value: 'third', text: 'III' },
             { value: 'fourth', text: 'IV' },
             { value: 'fifth', text: 'V' }
-        ];
+        ]);
 
-        var genders = [
+        var genders = decorate([
             { value: 'male', text: 'Male' },
             { value: 'female', text: 'Female' },
             { value: "", text: 'Not indicated' }
-        ];
+        ]);
+        
+        function decorate(textValueList) {
+            textValueList.keys = [];
+            textValueList.text = {};
+            angular.forEach(textValueList, function(item) {
+                textValueList.keys.push(item.value);
+                textValueList.text[item.value] = item.text;
+            });
+            return textValueList;
+        }
         
         return {
+            decorate: decorate,
             professionalStatus: professionalStatus,
             phoneTypes: phoneTypes,
             salutations: salutations,
