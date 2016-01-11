09af6986511ce16803f9f2bffa8535d823867449
Equan Simmonds <esimmonds@ncarb.org>
2016-01-11 15:03:35 -0500

    feat(module): add zendesk widget

---
+++ modules/zendesk/widget.js
@@ -0,0 +1,44 @@
+(function(angular, window) {
+	'use strict';
+
+	angular
+		.module('ncarb.services.zendesk.widget', ['services.config'])
+		.run(['configuration',
+			function(configuration) {
+
+				var uri = configuration.zendeskUri;
+
+				if (!uri) {
+					return;
+				}
+
+				window.zEmbed || function(e, t) {
+					var n, o, d, i, s, a = [],
+						r = document.createElement("iframe");
+					window.zEmbed = function() {
+						a.push(arguments)
+					}, window.zE = window.zE || window.zEmbed, r.src = "javascript:false", r.title = "", r.role = "presentation", (r.frameElement || r).style.cssText = "display: none", d = document.getElementsByTagName("script"), d = d[d.length - 1], d.parentNode.insertBefore(r, d), i = r.contentWindow, s = i.document;
+					try {
+						o = s
+					} catch (c) {
+						n = document.domain, r.src = 'javascript:var d=document.open();d.domain="' + n + '";void(0);', o = s
+					}
+					o.open()._l = function() {
+						var o = this.createElement("script");
+						n && (this.domain = n), o.id = "js-iframe-async", o.src = e, this.t = +new Date, this.zendeskHost = t, this.zEQueue = a, this.body.appendChild(o)
+					}, o.write('<body onload="document._l();">'), o.close()
+				}("https://assets.zendesk.com/embeddable_framework/main.js", uri);
+
+			}
+		])
+		.service('zendeskWidgetService', function() {
+			this.populateUser = function(name, email) {
+				window.zE(function() {
+					zE.identify({
+						name: name,
+						email: email
+					});
+				});
+			};
+		});
+})(angular, window);
