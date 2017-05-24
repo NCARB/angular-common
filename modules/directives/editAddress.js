14e510aeb02ac2acfcfb89fbe9537dadcbf209ba
Dave Anderson <danderson@ncarb.org>
2017-05-24 07:22:11 -0400

    feat(editAddress): attr streetOptional instead of cityStateOnly

--- modules/directives/editAddress.js
+++ modules/directives/editAddress.js
@@ -5,14 +5,14 @@
         .module('ncarb.directives')
         .directive('editAddress', editAddress)
         .constant('editAddressConfig', {});
-    
+
     editAddress.$inject = ['editAddressConfig', '_'];
-    
+
     function editAddress(editAddressConfig, _) {
-        
+
         controller.$inject = ['$scope'];
         link.$inject = ['scope', 'elem', 'attrs', 'form'];
-        
+
         return {
             restrict: 'E',
             template: '<div ng-form="addressForm" class="flex-container flex-gutters">\
@@ -20,7 +20,7 @@
         <select name="country" ng-required="!hidden" ng-options="country.id as country.name for country in (countries | orderBy: \'name\')" ng-model="address.countryId" ng-change="countryIdChanged()"></select>\
     </div>\
     <div class="flex-item" feedback>\
-        <input name="streetLine1" ng-required="!hidden && !onlyCityState" type="text" ng-attr-placeholder="{{streetLine1Placeholder}}" ng-model="address.streetLine1"/>\
+        <input name="streetLine1" ng-required="!hidden && !streetOptional" type="text" ng-attr-placeholder="{{streetLine1Placeholder}}" ng-model="address.streetLine1"/>\
     </div>\
     <div class="flex-item">\
         <input name="streetLine2" type="text" ng-attr-placeholder="{{streetLine2Placeholder}}" ng-model="address.streetLine2" />\
@@ -38,7 +38,7 @@
         <input type="text" ng-model="address.stateProvenceOrRegionText" placeholder="Region" />\
     </div>\
     <div class="flex-item flex-fill flex-1of4" feedback>\
-        <input ng-required="!hidden && !onlyCityState" name="zip" type="text" ng-model="address.zip" ng-attr-placeholder="IsUsAddress() ? \'Zip Code\' : \'Postal Code\'" placeholder="Postal Code"  zip-pattern-by-country-id />\
+        <input ng-required="!hidden" name="zip" type="text" ng-model="address.zip" ng-attr-placeholder="IsUsAddress() ? \'Zip Code\' : \'Postal Code\'" placeholder="Postal Code"  zip-pattern-by-country-id />\
     </div>\
 </div>',
             replace: true,
@@ -48,13 +48,13 @@
                 states: '=',
                 countries: '=',
                 hidden: '=ngHide',
-                onlyCityState:'=bind'
+                streetOptional:'=bind'
             },
             controller: controller,
             link: link
         };
-    
-    
+
+
         function controller($scope) {
             this.usa = $scope.usa = _.find($scope.countries, function(country) {
                 return country.code === 'USA';
@@ -65,7 +65,7 @@
             this.getAddress = function() {
                 return $scope.address;
             };
-    
+
             $scope.countryIdChanged = function() {
                 $scope.address.countryText = $scope.getCountryNameById($scope.address.countryId);
                 $scope.address.stateId = null;
@@ -73,14 +73,14 @@
                 $scope.address.stateProvinceOrRegionText = null;
                 $scope.$broadcast('countryIdChanged', $scope.address.countryId);
             };
-    
+
             $scope.stateIdChanged = function() {
                 var state = _.find($scope.states, function(state) {
                     return state.id === $scope.address.stateId;
                 });
                 $scope.address.stateProvinceOrRegionText = state ? state.code : null;
             };
-    
+
             $scope.provinceIdChanged = function() {
                 var province = _.find($scope.states, function(state) {
                     return state.id === $scope.address.provinceId;
@@ -88,31 +88,31 @@
                 $scope.address.stateProvinceOrRegionText = province ? province.code : null;
             };
         }
-        
-        
+
+
         function link(scope, elem, attrs, form) {
-            scope.streetLine1Placeholder = angular.isDefined(attrs.streetLine1Placeholder) 
+            scope.streetLine1Placeholder = angular.isDefined(attrs.streetLine1Placeholder)
                 ? attrs.streetLine1Placeholder
                 : angular.isDefined(editAddressConfig.streetLine1Placeholder)
                 ? editAddressConfig.streetLine1Placeholder
                 : "Street Line 1";
-            scope.streetLine2Placeholder = angular.isDefined(attrs.streetLine2Placeholder) 
+            scope.streetLine2Placeholder = angular.isDefined(attrs.streetLine2Placeholder)
                 ? attrs.streetLine2Placeholder
                 : angular.isDefined(editAddressConfig.streetLine2Placeholder)
                 ? editAddressConfig.streetLine2Placeholder
-                : scope.streetLine1Placeholder != "Street Line 1" 
-                ? "Street Line 1" 
+                : scope.streetLine1Placeholder != "Street Line 1"
+                ? "Street Line 1"
                 : "Street Line 2";
-            scope.onlyCityState = angular.isDefined(attrs.onlyCityState)
-                ? attrs.onlyCityState
-                : editAddressConfig.onlyCityState
-                
+            scope.streetOptional = angular.isDefined(attrs.streetOptional)
+                ? attrs.streetOptional
+                : editAddressConfig.streetOptional
+
             scope.IsUsAddress = function() {
                 return scope.address.countryId === scope.usa.id;
             };
             scope.IsCaAddress = function() {
                 return scope.address.countryId === scope.canada.id;
-            };          
+            };
             scope.IsForeignAddress = function() {
                 return !scope.IsUsAddress() && !scope.IsCaAddress();
             };
@@ -122,12 +122,12 @@
                     return country.id === countryId;
                 }).name;
             };
-    
+
             if(!scope.address.countryId) {
-                scope.address.countryId = scope.address.country 
+                scope.address.countryId = scope.address.country
                 ? scope.address.country.id
                 : scope.usa.id;
-    
+
                 scope.address.countryText = scope.getCountryNameById(scope.address.countryId);
             }
         }
